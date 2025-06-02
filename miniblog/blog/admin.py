401fec13c9490be2903f584cd2f45c73f31f523b
imzamam0651 <mdimzamamulhaue@gmail.com>
2025-06-02 15:39:26 +0530

    Registered models to admin.

--- miniblog/blog/admin.py
+++ miniblog/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from blog.models import Blog, Like, Comment
+
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Like)
+admin.site.register(Comment)
